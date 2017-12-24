1d3e8602983389a75e668e85a034908f45e5f40b
Alejandro Terrill D <alejandro.terrill@ceiba.com.co>
2017-12-24 15:41:42 -0500

    parking aterrill

--- src/main/java/com/ceiba/apis/parking/impl/ApiParking.java
+++ src/main/java/com/ceiba/apis/parking/impl/ApiParking.java
@@ -1,41 +1,39 @@
 package com.ceiba.apis.parking.impl;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ceiba.apis.parking.model.ApiResponse;
-import com.ceiba.apis.parking.model.Greeting;
 import com.ceiba.bl.parking.IParking;
+import com.ceiba.bl.parking.models.Parking;
 import com.ceiba.bl.parking.models.Vehicle;
 
 @RestController
-public class ApiParking{
-
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
+public class ApiParking {
     
     @Autowired
     IParking parkingImpl;
 
-    @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
-    }
+    @RequestMapping("/")
+    public ApiResponse root() {		
+		ApiResponse apiResponse = new ApiResponse();
+		apiResponse.setPayload("Api parqueadero");
+		return apiResponse;
+	}
 
+    @CrossOrigin(origins="*")
     @RequestMapping("/parking/{parkingId}/vehicles")
     public ApiResponse registerVehicleIn(@PathVariable(name="parkingId") String parkingId, @RequestBody Vehicle vehicle) {
 		
 		ApiResponse apiResponse = new ApiResponse();
 		try {
-			parkingImpl.registerVehicle(parkingId, vehicle);
+			apiResponse.setPayload(parkingImpl.registerVehicle(parkingId, vehicle));
 			apiResponse.setError(Boolean.FALSE);			
+			apiResponse.setMessage("Vehicle registered.");
 			return apiResponse;
 		} catch (Exception e) {
 			apiResponse.setError(Boolean.TRUE);
@@ -46,8 +44,9 @@ public class ApiParking{
 	}
 
     @RequestMapping("/parking/{parkingId}/bills/{licensePlate}")
-	public ApiResponse cashParking(@PathVariable(name="parkingId") String parkingId, @PathVariable(name="licensePlate") String licensePlate) {
-		// TODO Auto-generated method stub
+	public ApiResponse cashParking(@PathVariable(name="parkingId") String parkingId, 
+				@PathVariable(name="licensePlate") String licensePlate) {
+		
 		ApiResponse apiResponse = new ApiResponse();
 		
 		try {
@@ -56,18 +55,29 @@ public class ApiParking{
 			apiResponse.setMessage("okokokok");
 			return apiResponse;
 		} catch (Exception e) {
-			// TODO: handle exception
+			
 			apiResponse.setError(Boolean.TRUE);
 			e.printStackTrace();
 			apiResponse.setMessage("Exception: " + e.getMessage() +  e.getCause());
 			return apiResponse;
 		}
 	}
-
-	public ApiResponse root() {
+    
+    @CrossOrigin(origins="*")
+    @RequestMapping("/parking")
+    public ApiResponse registerParking(@RequestBody Parking parking) {
 		
 		ApiResponse apiResponse = new ApiResponse();
-		apiResponse.setPayload("Api parqueadero");
-		return apiResponse;
+		try {
+			parking = parkingImpl.registerParking(parking);
+			apiResponse.setError(Boolean.FALSE);	
+			apiResponse.setPayload(parking);
+			return apiResponse;
+		} catch (Exception e) {
+			apiResponse.setError(Boolean.TRUE);
+			apiResponse.setMessage(e.getMessage());
+			return apiResponse;			
+		}
+				
 	}
 }
